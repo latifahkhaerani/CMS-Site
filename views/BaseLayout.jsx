813402e8116e4383dc88992503c7561ace20447b
latifahkhaerani <khaeranifah@gmail.com>
2026-06-14 22:17:49 +0700

    auto redirtec

--- views/BaseLayout.jsx
+++ views/BaseLayout.jsx
@@ -1,15 +1,24 @@
-import { Outlet } from "react-router";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
 export default function BaseLayout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+
+    if (!token) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
-    <>
-      <div>
-        <Navbar />
-        <Sidebar/>
-        <Outlet />
-      </div>
-    </>
+    <div>
+      <Navbar />
+      <Sidebar />
+      <Outlet />
+    </div>
   );
 }
